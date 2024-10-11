b304e7299cd3432adee862083c35ff4282b8c368
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-10-11 10:27:25 +0800

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -49,7 +49,9 @@ __all__ = (
     "Attention",
     "PSA",
     "SCDown",
-    "ParNetAttention"
+    "ParNetAttention",
+    "Concat_BiFPN"
+
 )
 
 
@@ -1135,3 +1137,17 @@ class ParNetAttention(nn.Module):
         y = self.silu(x1 + x2 + x3)
         return y
 
+
+class Concat_BiFPN(nn.Module):
+    def __init__(self, dimension=1):
+        super(Concat_BiFPN, self).__init__()
+        self.d = dimension
+        self.w = nn.Parameter(torch.ones(3, dtype=torch.float32), requires_grad=True)
+        self.epsilon = 0.0001
+ 
+    def forward(self, x):
+        w = self.w
+        weight = w / (torch.sum(w, dim=0) + self.epsilon)  # 将权重进行归一化
+        # Fast normalized fusion
+        x = [weight[0] * x[0], weight[1] * x[1]]
+        return torch.cat(x, self.d)
