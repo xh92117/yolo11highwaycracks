f5816706b2bed3bbdd57c3b17dc9ddea615ae465
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-10-21 21:36:18 +0800

    Create DAttention.py

---
+++ ultralytics/nn/modules/DAttention.py
@@ -0,0 +1,352 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import einops
+from timm.models.layers import  trunc_normal_
+ 
+__all__ = ['DAttentionBaseline', 'C2PSA_DAT']
+ 
+ 
+class LayerNormProxy(nn.Module):
+ 
+    def __init__(self, dim):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+ 
+    def forward(self, x):
+        x = einops.rearrange(x, 'b c h w -> b h w c')
+        x = self.norm(x)
+        return einops.rearrange(x, 'b h w c -> b c h w')
+ 
+ 
+class DAttentionBaseline(nn.Module):
+ 
+    def __init__(
+            self, q_size=(224,224), kv_size=(224,224), n_heads=8, n_head_channels=32, n_groups=1,
+            attn_drop=0.0, proj_drop=0.0, stride=1,
+            offset_range_factor=-1, use_pe=True, dwc_pe=True,
+            no_off=False, fixed_pe=False, ksize=9, log_cpb=False
+    ):
+ 
+        super().__init__()
+        n_head_channels = int(q_size / 8)
+        q_size = (q_size, q_size)
+ 
+        self.dwc_pe = dwc_pe
+        self.n_head_channels = n_head_channels
+        self.scale = self.n_head_channels ** -0.5
+        self.n_heads = n_heads
+        self.q_h, self.q_w = q_size
+        # self.kv_h, self.kv_w = kv_size
+        self.kv_h, self.kv_w = self.q_h // stride, self.q_w // stride
+        self.nc = n_head_channels * n_heads
+        self.n_groups = n_groups
+        self.n_group_channels = self.nc // self.n_groups
+        self.n_group_heads = self.n_heads // self.n_groups
+        self.use_pe = use_pe
+        self.fixed_pe = fixed_pe
+        self.no_off = no_off
+        self.offset_range_factor = offset_range_factor
+        self.ksize = ksize
+        self.log_cpb = log_cpb
+        self.stride = stride
+        kk = self.ksize
+        pad_size = kk // 2 if kk != stride else 0
+ 
+ 
+        self.conv_offset = nn.Sequential(
+            nn.Conv2d(self.n_group_channels, self.n_group_channels, kk, stride, pad_size, groups=self.n_group_channels),
+            LayerNormProxy(self.n_group_channels),
+            nn.GELU(),
+            nn.Conv2d(self.n_group_channels, 2, 1, 1, 0, bias=False)
+        )
+ 
+        if self.no_off:
+            for m in self.conv_offset.parameters():
+                m.requires_grad_(False)
+ 
+        self.proj_q = nn.Conv2d(
+            self.nc, self.nc,
+            kernel_size=1, stride=1, padding=0
+        )
+ 
+        self.proj_k = nn.Conv2d(
+            self.nc, self.nc,
+            kernel_size=1, stride=1, padding=0)
+ 
+        self.proj_v = nn.Conv2d(
+            self.nc, self.nc,
+            kernel_size=1, stride=1, padding=0
+        )
+        self.proj_out = nn.Conv2d(
+            self.nc, self.nc,
+            kernel_size=1, stride=1, padding=0
+        )
+ 
+        self.proj_drop = nn.Dropout(proj_drop, inplace=True)
+        self.attn_drop = nn.Dropout(attn_drop, inplace=True)
+ 
+        if self.use_pe and not self.no_off:
+            if self.dwc_pe:
+                self.rpe_table = nn.Conv2d(
+                    self.nc, self.nc, kernel_size=3, stride=1, padding=1, groups=self.nc)
+            elif self.fixed_pe:
+                self.rpe_table = nn.Parameter(
+                    torch.zeros(self.n_heads, self.q_h * self.q_w, self.kv_h * self.kv_w)
+                )
+                trunc_normal_(self.rpe_table, std=0.01)
+            elif self.log_cpb:
+                # Borrowed from Swin-V2
+                self.rpe_table = nn.Sequential(
+                    nn.Linear(2, 32, bias=True),
+                    nn.ReLU(inplace=True),
+                    nn.Linear(32, self.n_group_heads, bias=False)
+                )
+            else:
+                self.rpe_table = nn.Parameter(
+                    torch.zeros(self.n_heads, self.q_h * 2 - 1, self.q_w * 2 - 1)
+                )
+                trunc_normal_(self.rpe_table, std=0.01)
+        else:
+            self.rpe_table = None
+ 
+    @torch.no_grad()
+    def _get_ref_points(self, H_key, W_key, B, dtype, device):
+ 
+        ref_y, ref_x = torch.meshgrid(
+            torch.linspace(0.5, H_key - 0.5, H_key, dtype=dtype, device=device),
+            torch.linspace(0.5, W_key - 0.5, W_key, dtype=dtype, device=device),
+            indexing='ij'
+        )
+        ref = torch.stack((ref_y, ref_x), -1)
+        ref[..., 1].div_(W_key - 1.0).mul_(2.0).sub_(1.0)
+        ref[..., 0].div_(H_key - 1.0).mul_(2.0).sub_(1.0)
+        ref = ref[None, ...].expand(B * self.n_groups, -1, -1, -1)  # B * g H W 2
+ 
+        return ref
+ 
+    @torch.no_grad()
+    def _get_q_grid(self, H, W, B, dtype, device):
+ 
+        ref_y, ref_x = torch.meshgrid(
+            torch.arange(0, H, dtype=dtype, device=device),
+            torch.arange(0, W, dtype=dtype, device=device),
+            indexing='ij'
+        )
+        ref = torch.stack((ref_y, ref_x), -1)
+        ref[..., 1].div_(W - 1.0).mul_(2.0).sub_(1.0)
+        ref[..., 0].div_(H - 1.0).mul_(2.0).sub_(1.0)
+        ref = ref[None, ...].expand(B * self.n_groups, -1, -1, -1)  # B * g H W 2
+ 
+        return ref
+ 
+    def forward(self, x):
+        x = x
+        B, C, H, W = x.size()
+        dtype, device = x.dtype, x.device
+ 
+        q = self.proj_q(x)
+        q_off = einops.rearrange(q, 'b (g c) h w -> (b g) c h w', g=self.n_groups, c=self.n_group_channels)
+        offset = self.conv_offset(q_off).contiguous()  # B * g 2 Hg Wg
+        Hk, Wk = offset.size(2), offset.size(3)
+        n_sample = Hk * Wk
+ 
+        if self.offset_range_factor >= 0 and not self.no_off:
+            offset_range = torch.tensor([1.0 / (Hk - 1.0), 1.0 / (Wk - 1.0)], device=device).reshape(1, 2, 1, 1)
+            offset = offset.tanh().mul(offset_range).mul(self.offset_range_factor)
+ 
+        offset = einops.rearrange(offset, 'b p h w -> b h w p')
+        reference = self._get_ref_points(Hk, Wk, B, dtype, device)
+ 
+        if self.no_off:
+            offset = offset.fill_(0.0)
+ 
+        if self.offset_range_factor >= 0:
+            pos = offset + reference
+        else:
+            pos = (offset + reference).clamp(-1., +1.)
+ 
+        if self.no_off:
+            x_sampled = F.avg_pool2d(x, kernel_size=self.stride, stride=self.stride)
+            assert x_sampled.size(2) == Hk and x_sampled.size(3) == Wk, f"Size is {x_sampled.size()}"
+        else:
+            x_sampled = F.grid_sample(
+                input=x.reshape(B * self.n_groups, self.n_group_channels, H, W),
+                grid=pos[..., (1, 0)],  # y, x -> x, y
+                mode='bilinear', align_corners=True)  # B * g, Cg, Hg, Wg
+ 
+        x_sampled = x_sampled.reshape(B, C, 1, n_sample)
+        # self.proj_k.weight = torch.nn.Parameter(self.proj_k.weight.float())
+        # self.proj_k.bias = torch.nn.Parameter(self.proj_k.bias.float())
+        # self.proj_v.weight = torch.nn.Parameter(self.proj_v.weight.float())
+        # self.proj_v.bias = torch.nn.Parameter(self.proj_v.bias.float())
+        # 检查权重的数据类型
+        q = q.reshape(B * self.n_heads, self.n_head_channels, H * W)
+ 
+        k = self.proj_k(x_sampled).reshape(B * self.n_heads, self.n_head_channels, n_sample)
+        v = self.proj_v(x_sampled).reshape(B * self.n_heads, self.n_head_channels, n_sample)
+ 
+        attn = torch.einsum('b c m, b c n -> b m n', q, k)  # B * h, HW, Ns
+        attn = attn.mul(self.scale)
+ 
+        if self.use_pe and (not self.no_off):
+ 
+            if self.dwc_pe:
+                residual_lepe = self.rpe_table(q.reshape(B, C, H, W)).reshape(B * self.n_heads, self.n_head_channels,
+                                                                              H * W)
+            elif self.fixed_pe:
+                rpe_table = self.rpe_table
+                attn_bias = rpe_table[None, ...].expand(B, -1, -1, -1)
+                attn = attn + attn_bias.reshape(B * self.n_heads, H * W, n_sample)
+            elif self.log_cpb:
+                q_grid = self._get_q_grid(H, W, B, dtype, device)
+                displacement = (
+                            q_grid.reshape(B * self.n_groups, H * W, 2).unsqueeze(2) - pos.reshape(B * self.n_groups,
+                                                                                                   n_sample,
+                                                                                                   2).unsqueeze(1)).mul(
+                    4.0)  # d_y, d_x [-8, +8]
+                displacement = torch.sign(displacement) * torch.log2(torch.abs(displacement) + 1.0) / np.log2(8.0)
+                attn_bias = self.rpe_table(displacement)  # B * g, H * W, n_sample, h_g
+                attn = attn + einops.rearrange(attn_bias, 'b m n h -> (b h) m n', h=self.n_group_heads)
+            else:
+                rpe_table = self.rpe_table
+                rpe_bias = rpe_table[None, ...].expand(B, -1, -1, -1)
+                q_grid = self._get_q_grid(H, W, B, dtype, device)
+                displacement = (
+                            q_grid.reshape(B * self.n_groups, H * W, 2).unsqueeze(2) - pos.reshape(B * self.n_groups,
+                                                                                                   n_sample,
+                                                                                                   2).unsqueeze(1)).mul(
+                    0.5)
+                attn_bias = F.grid_sample(
+                    input=einops.rearrange(rpe_bias, 'b (g c) h w -> (b g) c h w', c=self.n_group_heads,
+                                           g=self.n_groups),
+                    grid=displacement[..., (1, 0)],
+                    mode='bilinear', align_corners=True)  # B * g, h_g, HW, Ns
+ 
+                attn_bias = attn_bias.reshape(B * self.n_heads, H * W, n_sample)
+                attn = attn + attn_bias
+ 
+        attn = F.softmax(attn, dim=2)
+        attn = self.attn_drop(attn)
+ 
+        out = torch.einsum('b m n, b c n -> b c m', attn, v)
+ 
+        if self.use_pe and self.dwc_pe:
+            out = out + residual_lepe
+        out = out.reshape(B, C, H, W)
+ 
+        y = self.proj_drop(self.proj_out(out))
+        h, w = pos.reshape(B, self.n_groups, Hk, Wk, 2), reference.reshape(B, self.n_groups, Hk, Wk, 2)
+ 
+        return y
+ 
+ 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+ 
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+ 
+class PSABlock(nn.Module):
+    """
+    PSABlock class implementing a Position-Sensitive Attention block for neural networks.
+    This class encapsulates the functionality for applying multi-head attention and feed-forward neural network layers
+    with optional shortcut connections.
+    Attributes:
+        attn (Attention): Multi-head attention module.
+        ffn (nn.Sequential): Feed-forward neural network module.
+        add (bool): Flag indicating whether to add shortcut connections.
+    Methods:
+        forward: Performs a forward pass through the PSABlock, applying attention and feed-forward layers.
+    Examples:
+        Create a PSABlock and perform a forward pass
+        >>> psablock = PSABlock(c=128, attn_ratio=0.5, num_heads=4, shortcut=True)
+        >>> input_tensor = torch.randn(1, 128, 32, 32)
+        >>> output_tensor = psablock(input_tensor)
+    """
+ 
+    def __init__(self, c, attn_ratio=0.5, num_heads=4, shortcut=True) -> None:
+        """Initializes the PSABlock with attention and feed-forward layers for enhanced feature extraction."""
+        super().__init__()
+ 
+        self.attn = DAttentionBaseline(c)
+        self.ffn = nn.Sequential(Conv(c, c * 2, 1), Conv(c * 2, c, 1, act=False))
+        self.add = shortcut
+ 
+    def forward(self, x):
+        """Executes a forward pass through PSABlock, applying attention and feed-forward layers to the input tensor."""
+        x = x + self.attn(x) if self.add else self.attn(x)
+        x = x + self.ffn(x) if self.add else self.ffn(x)
+        return x
+ 
+ 
+class C2PSA_DAT(nn.Module):
+    """
+    C2PSA module with attention mechanism for enhanced feature extraction and processing.
+    This module implements a convolutional block with attention mechanisms to enhance feature extraction and processing
+    capabilities. It includes a series of PSABlock modules for self-attention and feed-forward operations.
+    Attributes:
+        c (int): Number of hidden channels.
+        cv1 (Conv): 1x1 convolution layer to reduce the number of input channels to 2*c.
+        cv2 (Conv): 1x1 convolution layer to reduce the number of output channels to c.
+        m (nn.Sequential): Sequential container of PSABlock modules for attention and feed-forward operations.
+    Methods:
+        forward: Performs a forward pass through the C2PSA module, applying attention and feed-forward operations.
+    Notes:
+        This module essentially is the same as PSA module, but refactored to allow stacking more PSABlock modules.
+    Examples:
+        >>> c2psa = C2PSA(c1=256, c2=256, n=3, e=0.5)
+        >>> input_tensor = torch.randn(1, 256, 64, 64)
+        >>> output_tensor = c2psa(input_tensor)
+    """
+ 
+    def __init__(self, c1, c2, n=1, e=0.5):
+        """Initializes the C2PSA module with specified input/output channels, number of layers, and expansion ratio."""
+        super().__init__()
+        assert c1 == c2
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+ 
+        self.m = nn.Sequential(*(PSABlock(self.c, attn_ratio=0.5, num_heads=self.c // 64) for _ in range(n)))
+ 
+    def forward(self, x):
+        """Processes the input tensor 'x' through a series of PSA blocks and returns the transformed tensor."""
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = self.m(b)
+        return self.cv2(torch.cat((a, b), 1))
+ 
+if __name__ == "__main__":
+    # Generating Sample image
+    image_size = (1, 64, 224, 224)
+    image = torch.rand(*image_size)
+ 
+    # Model
+    model = C2PSA_DAT(64, 64)
+ 
+    out = model(image)
+    print(out.size())
