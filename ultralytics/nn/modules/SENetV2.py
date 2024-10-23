1b05b70ce59b19f50164da081a91821d05db9125
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-10-23 17:49:31 +0800

    Create SENetV2.py

---
+++ ultralytics/nn/modules/SENetV2.py
@@ -0,0 +1,203 @@
+import torch
+import torch.nn as nn
+ 
+__all__ = ['C2PSA_SENetV2', 'SELayerV2', 'SPPFSENetV2']
+ 
+# 定义SE模块
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+ 
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+ 
+ 
+# 定义SaE模块
+class SELayerV2(nn.Module):
+    def __init__(self, in_channel, reduction=16):
+        super(SELayerV2, self).__init__()
+        assert in_channel >= reduction and in_channel % reduction == 0, 'invalid in_channel in SaElayer'
+        self.reduction = reduction
+        self.cardinality = 4
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # cardinality 1
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+        # cardinality 2
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+        # cardinality 3
+        self.fc3 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+        # cardinality 4
+        self.fc4 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+ 
+        self.fc = nn.Sequential(
+            nn.Linear(in_channel // self.reduction * self.cardinality, in_channel, bias=False),
+            nn.Sigmoid()
+        )
+ 
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y1 = self.fc1(y)
+        y2 = self.fc2(y)
+        y3 = self.fc3(y)
+        y4 = self.fc4(y)
+        y_concate = torch.cat([y1, y2, y3, y4], dim=1)
+        y_ex_dim = self.fc(y_concate).view(b, c, 1, 1)
+ 
+        return x * y_ex_dim.expand_as(x)
+ 
+ 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+ 
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+ 
+class PSABlock(nn.Module):
+    """
+    PSABlock class implementing a Position-Sensitive Attention block for neural networks.
+    This class encapsulates the functionality for applying multi-head attention and feed-forward neural network layers
+    with optional shortcut connections.
+    Attributes:
+        attn (Attention): Multi-head attention module.
+        ffn (nn.Sequential): Feed-forward neural network module.
+        add (bool): Flag indicating whether to add shortcut connections.
+    Methods:
+        forward: Performs a forward pass through the PSABlock, applying attention and feed-forward layers.
+    Examples:
+        Create a PSABlock and perform a forward pass
+        >>> psablock = PSABlock(c=128, attn_ratio=0.5, num_heads=4, shortcut=True)
+        >>> input_tensor = torch.randn(1, 128, 32, 32)
+        >>> output_tensor = psablock(input_tensor)
+    """
+ 
+    def __init__(self, c, attn_ratio=0.5, num_heads=4, shortcut=True) -> None:
+        """Initializes the PSABlock with attention and feed-forward layers for enhanced feature extraction."""
+        super().__init__()
+ 
+        self.attn = SELayerV2(c)
+        self.ffn = nn.Sequential(Conv(c, c * 2, 1), Conv(c * 2, c, 1, act=False))
+        self.add = shortcut
+ 
+    def forward(self, x):
+        """Executes a forward pass through PSABlock, applying attention and feed-forward layers to the input tensor."""
+        x = x + self.attn(x) if self.add else self.attn(x)
+        x = x + self.ffn(x) if self.add else self.ffn(x)
+        return x
+ 
+ 
+class C2PSA_SENetV2(nn.Module):
+    """
+    C2PSA module with attention mechanism for enhanced feature extraction and processing.
+    This module implements a convolutional block with attention mechanisms to enhance feature extraction and processing
+    capabilities. It includes a series of PSABlock modules for self-attention and feed-forward operations.
+    Attributes:
+        c (int): Number of hidden channels.
+        cv1 (Conv): 1x1 convolution layer to reduce the number of input channels to 2*c.
+        cv2 (Conv): 1x1 convolution layer to reduce the number of output channels to c.
+        m (nn.Sequential): Sequential container of PSABlock modules for attention and feed-forward operations.
+    Methods:
+        forward: Performs a forward pass through the C2PSA module, applying attention and feed-forward operations.
+    Notes:
+        This module essentially is the same as PSA module, but refactored to allow stacking more PSABlock modules.
+    Examples:
+        >>> c2psa = C2PSA(c1=256, c2=256, n=3, e=0.5)
+        >>> input_tensor = torch.randn(1, 256, 64, 64)
+        >>> output_tensor = c2psa(input_tensor)
+    """
+ 
+    def __init__(self, c1, c2, n=1, e=0.5):
+        """Initializes the C2PSA module with specified input/output channels, number of layers, and expansion ratio."""
+        super().__init__()
+        assert c1 == c2
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+ 
+        self.m = nn.Sequential(*(PSABlock(self.c, attn_ratio=0.5, num_heads=self.c // 64) for _ in range(n)))
+ 
+    def forward(self, x):
+        """Processes the input tensor 'x' through a series of PSA blocks and returns the transformed tensor."""
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = self.m(b)
+        return self.cv2(torch.cat((a, b), 1))
+ 
+ 
+class SPPFSENetV2(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
+ 
+    def __init__(self, c1, c2, k=5):
+        """
+        Initializes the SPPF layer with given input/output channels and kernel size.
+        This module is equivalent to SPP(k=(5, 9, 13)).
+        """
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+        self.h = nn.AvgPool2d(kernel_size=k, stride=1, padding=k // 2)
+        self.Att = SELayerV2(c1)
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        x = self.Att(x)
+        y = [self.cv1(x)]
+        y.extend(self.m(y[-1]) for _ in range(3))
+        return self.cv2(torch.cat(y, 1))
+ 
+ 
+if __name__ == "__main__":
+    # Generating Sample image
+    image_size = (1, 64, 240, 240)
+    image = torch.rand(*image_size)
+ 
+    # Model
+    mobilenet_v1 = SPPFSENetV2(64, 64)
+ 
+    out = mobilenet_v1(image)
+    print(out.size())
