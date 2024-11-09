0cf565f4a11780ca41351e1a3a0adec9364e966b
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-11-09 10:51:52 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -35,7 +35,7 @@ from ultralytics.nn.modules.SENetV2 import *
 from ultralytics.nn.modules.C3k2_DAttention import *
 
 
-from ultralytics.nn.modules.DynamicHead import *
+# from ultralytics.nn.modules.DynamicHead import *
 
 
 from ultralytics.nn.modules.AFPNHead import *
@@ -367,7 +367,7 @@ class BaseModel(nn.Module):
         """
         self = super()._apply(fn)
         m = self.model[-1]  # Detect()
-        if isinstance(m, (Detect,ASFFHead, Detect_ASFF,AFPN4Head,AFPNHead, DynamicHead)):  # includes all Detect subclasses like Segment, Pose, OBB, WorldDetect
+        if isinstance(m, (Detect,ASFFHead, Detect_ASFF,AFPN4Head,AFPNHead)):  # includes all Detect subclasses like Segment, Pose, OBB, WorldDetect
             m.stride = fn(m.stride)
             m.anchors = fn(m.anchors)
             m.strides = fn(m.strides)
@@ -1253,7 +1253,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args = [ch[f]]
         elif m is Concat:
             c2 = sum(ch[x] for x in f)
-        elif m in {Detect, WorldDetect, Segment, Pose, OBB, ImagePoolingAttn, v10Detect,ASFFHead, Detect_ASFF, AFPN4Head, AFPNHead,DynamicHead}:
+        elif m in {Detect, WorldDetect, Segment, Pose, OBB, ImagePoolingAttn, v10Detect,ASFFHead, Detect_ASFF, AFPN4Head, AFPNHead}:
             args.append([ch[x] for x in f])
             if m is Segment:
                 args[2] = make_divisible(min(args[2], max_channels) * width, 8)
@@ -1430,7 +1430,7 @@ def guess_model_task(model):
                 return "pose"
             elif isinstance(m, OBB):
                 return "obb"
-            elif isinstance(m, (Detect, WorldDetect, v10Detect,ASFFHead,Detect_ASFF,AFPN4Head, AFPNHead,DynamicHead)):
+            elif isinstance(m, (Detect, WorldDetect, v10Detect,ASFFHead,Detect_ASFF,AFPN4Head, AFPNHead)):
                 return "detect"
 
     # Guess from model filename
