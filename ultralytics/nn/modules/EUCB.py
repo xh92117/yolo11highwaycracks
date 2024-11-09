d9ce0b00a8af4da0f4f28b00d76692d7132fea69
scuzyq <142667711+scuzyq@users.noreply.github.com>
2024-11-09 17:03:45 +0800

    Create EUCB.py

---
+++ ultralytics/nn/modules/EUCB.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+from functools import partial
+import math
+from timm.models.layers import trunc_normal_tf_
+from timm.models.helpers import named_apply
+ 
+__all__ = ['EUCB']
+ 
+ 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+ 
+ 
+# Other types of layers can go here (e.g., nn.Linear, etc.)
+def _init_weights(module, name, scheme=''):
+    if isinstance(module, nn.Conv2d) or isinstance(module, nn.Conv3d):
+        if scheme == 'normal':
+            nn.init.normal_(module.weight, std=.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif scheme == 'trunc_normal':
+            trunc_normal_tf_(module.weight, std=.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif scheme == 'xavier_normal':
+            nn.init.xavier_normal_(module.weight)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif scheme == 'kaiming_normal':
+            nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        else:
+            # efficientnet like
+            fan_out = module.kernel_size[0] * module.kernel_size[1] * module.out_channels
+            fan_out //= module.groups
+            nn.init.normal_(module.weight, 0, math.sqrt(2.0 / fan_out))
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.BatchNorm2d) or isinstance(module, nn.BatchNorm3d):
+        nn.init.constant_(module.weight, 1)
+        nn.init.constant_(module.bias, 0)
+    elif isinstance(module, nn.LayerNorm):
+        nn.init.constant_(module.weight, 1)
+        nn.init.constant_(module.bias, 0)
+ 
+ 
+def act_layer(act, inplace=False, neg_slope=0.2, n_prelu=1):
+    # activation layer
+    act = act.lower()
+    if act == 'relu':
+        layer = nn.ReLU(inplace)
+    elif act == 'relu6':
+        layer = nn.ReLU6(inplace)
+    elif act == 'leakyrelu':
+        layer = nn.LeakyReLU(neg_slope, inplace)
+    elif act == 'prelu':
+        layer = nn.PReLU(num_parameters=n_prelu, init=neg_slope)
+    elif act == 'gelu':
+        layer = nn.GELU()
+    elif act == 'hswish':
+        layer = nn.Hardswish(inplace)
+    else:
+        raise NotImplementedError('activation layer [%s] is not found' % act)
+    return layer
+ 
+ 
+def channel_shuffle(x, groups):
+    batchsize, num_channels, height, width = x.data.size()
+    channels_per_group = num_channels // groups
+    # reshape
+    x = x.view(batchsize, groups,
+               channels_per_group, height, width)
+    x = torch.transpose(x, 1, 2).contiguous()
+    # flatten
+    x = x.view(batchsize, -1, height, width)
+    return x
+ 
+ 
+#   Multi-scale depth-wise convolution (MSDC)
+class MSDC(nn.Module):
+    def __init__(self, in_channels, kernel_sizes=3, stride=1, activation='relu6', dw_parallel=True):
+        super(MSDC, self).__init__()
+ 
+        self.in_channels = in_channels
+        self.kernel_sizes = kernel_sizes
+        self.activation = activation
+        self.dw_parallel = dw_parallel
+ 
+        self.dwconvs = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv2d(self.in_channels, self.in_channels, kernel_size, stride, kernel_size // 2,
+                          groups=self.in_channels, bias=False),
+                nn.BatchNorm2d(self.in_channels),
+                act_layer(self.activation, inplace=True)
+            )
+            for kernel_size in self.kernel_sizes
+        ])
+ 
+        self.init_weights('normal')
+ 
+    def init_weights(self, scheme=''):
+        named_apply(partial(_init_weights, scheme=scheme), self)
+ 
+    def forward(self, x):
+        # Apply the convolution layers in a loop
+        outputs = []
+        for dwconv in self.dwconvs:
+            dw_out = dwconv(x)
+            outputs.append(dw_out)
+            if self.dw_parallel == False:
+                x = x + dw_out
+        # You can return outputs based on what you intend to do with them
+        return outputs
+ 
+ 
+ 
+ 
+#   Efficient up-convolution block (EUCB)
+class EUCB(nn.Module):
+    def __init__(self, in_channels, kernel_size=3, stride=1, activation='relu'):
+        super(EUCB, self).__init__()
+ 
+        self.in_channels = in_channels
+        self.out_channels = in_channels
+        self.up_dwc = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(self.in_channels, self.in_channels, kernel_size=kernel_size, stride=stride,
+                      padding=kernel_size // 2, groups=self.in_channels, bias=False),
+            nn.BatchNorm2d(self.in_channels),
+            act_layer(activation, inplace=True)
+        )
+        self.pwc = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1, stride=1, padding=0, bias=True)
+        )
+        self.init_weights('normal')
+ 
+    def init_weights(self, scheme=''):
+        named_apply(partial(_init_weights, scheme=scheme), self)
+ 
+    def forward(self, x):
+        x = self.up_dwc(x)
+        x = channel_shuffle(x, self.in_channels)
+        x = self.pwc(x)
+        return x
+ 
+ 
+ 
+if __name__ == "__main__":
+    # Generating Sample image
+    image_size = (1, 64, 240, 240)
+    image = torch.rand(*image_size)
+ 
+    # Model
+    mobilenet_v1 = EUCB(64)
+ 
+    out = mobilenet_v1(image)
+    print(out.size())
