d39d670ae7575c6294ea2ef5758eaf3d8c575e37
scuzyq <142667711+scuzyq@users.noreply.github.com>
2025-02-17 10:03:06 +0800

    Create ESSAttn.py

---
+++ ultralytics/nn/modules/ESSAttn.py
@@ -0,0 +1,119 @@
+import math
+import torch
+import torch.nn as nn
+from ultralytics.nn.modules.conv import autopad, Conv
+__all__ = ['C3k2_ESSAttn']
+class ESSAttn(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.lnqkv = nn.Linear(dim, dim * 3)
+        self.ln = nn.Linear(dim, dim)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        x = x.reshape(B, C, H * W).permute(0, 2, 1)
+        B, N, C = x.shape
+        qkv = self.lnqkv(x)
+        qkv = torch.split(qkv, C, 2)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        a = torch.mean(q, dim=2, keepdim=True)
+        q = q - a
+        a = torch.mean(k, dim=2, keepdim=True)
+        k = k - a
+        q2 = torch.pow(q, 2)
+        q2s = torch.sum(q2, dim=2, keepdim=True)
+        k2 = torch.pow(k, 2)
+        k2s = torch.sum(k2, dim=2, keepdim=True)
+        t1 = v
+        k2 = torch.nn.functional.normalize((k2 / (k2s + 1e-7)), dim=-2)
+        q2 = torch.nn.functional.normalize((q2 / (q2s + 1e-7)), dim=-1)
+        t2 = q2 @ (k2.transpose(-2, -1) @ v) / math.sqrt(N)
+        attn = t1 + t2
+        attn = self.ln(attn)
+        x = attn.reshape(B, H, W, C).permute(0, 3, 1, 2)
+        return x
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+ 
+ 
+class Bottleneck(nn.Module):
+    """Standard bottleneck."""
+ 
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+ 
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+ 
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class C2f_ESSAttn(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.att=ESSAttn(c2)
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        out=self.cv2(torch.cat(y, 1))
+        out=out+self.att(out)
+        return out
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        out=self.cv2(torch.cat(y, 1))
+        out=out+self.att(out)
+        return out
+
+class C3k2_ESSAttn(C2f_ESSAttn):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(
+            C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck(self.c, self.c, shortcut, g) for _ in range(n)
+        )
