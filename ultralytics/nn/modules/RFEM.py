579c376a6eafa8ef07aca8120dd4fd45d211b1e8
scuzyq <142667711+scuzyq@users.noreply.github.com>
2025-02-17 17:43:50 +0800

    Update RFEM.py

--- ultralytics/nn/modules/RFEM.py
+++ ultralytics/nn/modules/RFEM.py
@@ -1,1 +1,180 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+from torch.jit import Final
+import math
+import numpy as np
+from functools import partial
+from typing import Optional, Callable, Union
+from einops import rearrange, reduce
+from ultralytics.nn.modules.conv import *
+from ultralytics.nn.modules.block import C2f,C3
+
+__all__ =['C3k2_RFEM']
+
+
+
+class TridentBlock(nn.Module):
+    def __init__(self, c1, c2, stride=1, c=False, e=0.5, padding=[1, 2, 3], dilate=[1, 2, 3], bias=False):
+        super(TridentBlock, self).__init__()
+        self.stride = stride
+        self.c = c
+        c_ = int(c2 * e)
+        self.padding = padding
+        self.dilate = dilate
+        self.share_weightconv1 = nn.Parameter(torch.Tensor(c_, c1, 1, 1))
+        self.share_weightconv2 = nn.Parameter(torch.Tensor(c2, c_, 3, 3))
+
+        self.bn1 = nn.BatchNorm2d(c_)
+        self.bn2 = nn.BatchNorm2d(c2)
+
+        self.act = nn.SiLU()
+
+        nn.init.kaiming_uniform_(self.share_weightconv1, nonlinearity="relu")
+        nn.init.kaiming_uniform_(self.share_weightconv2, nonlinearity="relu")
+
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(c2))
+        else:
+            self.bias = None
+
+        if self.bias is not None:
+            nn.init.constant_(self.bias, 0)
+
+    def forward_for_small(self, x):
+        residual = x
+        out = nn.functional.conv2d(x, self.share_weightconv1, bias=self.bias)
+        out = self.bn1(out)
+        out = self.act(out)
+
+        out = nn.functional.conv2d(out, self.share_weightconv2, bias=self.bias, stride=self.stride,
+                                   padding=self.padding[0],
+                                   dilation=self.dilate[0])
+        out = self.bn2(out)
+        out += residual
+        out = self.act(out)
+
+        return out
+
+    def forward_for_middle(self, x):
+        residual = x
+        out = nn.functional.conv2d(x, self.share_weightconv1, bias=self.bias)
+        out = self.bn1(out)
+        out = self.act(out)
+
+        out = nn.functional.conv2d(out, self.share_weightconv2, bias=self.bias, stride=self.stride,
+                                   padding=self.padding[1],
+                                   dilation=self.dilate[1])
+        out = self.bn2(out)
+        out += residual
+        out = self.act(out)
+
+        return out
+
+    def forward_for_big(self, x):
+        residual = x
+        out = nn.functional.conv2d(x, self.share_weightconv1, bias=self.bias)
+        out = self.bn1(out)
+        out = self.act(out)
+        out = nn.functional.conv2d(out, self.share_weightconv2, bias=self.bias, stride=self.stride,
+                                   padding=self.padding[2],
+                                   dilation=self.dilate[2])
+        out = self.bn2(out)
+        out += residual
+        out = self.act(out)
+
+        return out
+
+    def forward(self, x):
+        xm = x
+        base_feat = []
+        if self.c is not False:
+            x1 = self.forward_for_small(x)
+            x2 = self.forward_for_middle(x)
+            x3 = self.forward_for_big(x)
+        else:
+            x1 = self.forward_for_small(xm[0])
+            x2 = self.forward_for_middle(xm[1])
+            x3 = self.forward_for_big(xm[2])
+
+        base_feat.append(x1)
+        base_feat.append(x2)
+        base_feat.append(x3)
+
+        return base_feat
+
+
+class RFEM(nn.Module):
+    def __init__(self, c1, c2, n=1, e=0.5, stride=1):
+        super(RFEM, self).__init__()
+        c = True
+        layers = []
+        layers.append(TridentBlock(c1, c2, stride=stride, c=c, e=e))
+        c1 = c2
+        for i in range(1, n):
+            layers.append(TridentBlock(c1, c2))
+        self.layer = nn.Sequential(*layers)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU()
+
+    def forward(self, x):
+        out = self.layer(x)
+        out = out[0] + out[1] + out[2] + x
+        out = self.act(self.bn(out))
+        return out
+
+
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+class C3k2_RFEM(C2f):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(
+            C3k(self.c, self.c, 2, shortcut, g) if c3k else RFEM(self.c, self.c) for _ in range(n)
+        )
+
+
+class C3k_RFEM(C3):
+    """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
+        """Initializes the C3k module with specified channels, number of layers, and configurations."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(RFEM(c_, c_) for _ in range(n)))
+
+
+
 
