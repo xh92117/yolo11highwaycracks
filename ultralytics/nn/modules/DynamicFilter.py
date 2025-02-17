c04ee9104664e87c2e0a7d051f548ad50f68fe2c
scuzyq <142667711+scuzyq@users.noreply.github.com>
2025-02-17 16:59:24 +0800

    Update DynamicFilter.py

--- ultralytics/nn/modules/DynamicFilter.py
+++ ultralytics/nn/modules/DynamicFilter.py
@@ -12,6 +12,8 @@ from ultralytics.nn.modules.conv import Conv, DWConv, RepConv, GhostConv, autopa
 from ultralytics.nn.modules.block import C3k2,C3k
 from ultralytics.nn.modules.metaformer.py import MetaFormerBlock
 
+__all__ =['C3k2_DynamicFilter']
+
 def resize_complex_weight(origin_weight, new_h, new_w):
     h, w, num_heads = origin_weight.shape[0:3]  # size, w, c, 2
     origin_weight = origin_weight.reshape(1, h, w, num_heads * 2).permute(0, 3, 1, 2)
@@ -118,6 +120,38 @@ class DynamicFilter(nn.Module):
         x = self.act2(x)
         x = self.pwconv2(x)
         return x
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+
 
 class C3k_DynamicFilter(C3k):
     def __init__(self, c1, c2, n=1, size=None, shortcut=False, g=1, e=0.5, k=3):
