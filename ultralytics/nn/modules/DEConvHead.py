23ff919d606be42c0d5ae2f95cb56575025ba4b5
scuzyq <142667711+scuzyq@users.noreply.github.com>
2025-02-20 15:27:02 +0800

    Create DEConvHead.py

---
+++ ultralytics/nn/modules/DEConvHead.py
@@ -0,0 +1,295 @@
+import math
+import torch
+from torch import nn
+from einops.layers.torch import Rearrange
+
+# 论文：DEA-Net: Single image dehazing based on detail-enhanced convolution and content-guided attention
+# Github地址：https://github.com/cecret3350/DEA-Net
+__all__=['Detect_DEConvHead']
+class Conv2d_cd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False, theta=1.0):
+        super(Conv2d_cd, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+        self.theta = theta
+
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight = Rearrange('c_in c_out k1 k2 -> c_in c_out (k1 k2)')(conv_weight)
+        conv_weight_cd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 3 * 3).fill_(0)
+        conv_weight_cd[:, :, :] = conv_weight[:, :, :]
+        conv_weight_cd[:, :, 4] = conv_weight[:, :, 4] - conv_weight[:, :, :].sum(2)
+        conv_weight_cd = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[3])(
+            conv_weight_cd)
+        return conv_weight_cd, self.conv.bias
+
+
+class Conv2d_ad(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False, theta=1.0):
+        super(Conv2d_ad, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+        self.theta = theta
+
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight = Rearrange('c_in c_out k1 k2 -> c_in c_out (k1 k2)')(conv_weight)
+        conv_weight_ad = conv_weight - self.theta * conv_weight[:, :, [3, 0, 1, 6, 4, 2, 7, 8, 5]]
+        conv_weight_ad = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[3])(
+            conv_weight_ad)
+        return conv_weight_ad, self.conv.bias
+
+
+class Conv2d_rd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=2, dilation=1, groups=1, bias=False, theta=1.0):
+
+        super(Conv2d_rd, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+        self.theta = theta
+
+    def forward(self, x):
+
+        if math.fabs(self.theta - 0.0) < 1e-8:
+            out_normal = self.conv(x)
+            return out_normal
+        else:
+            conv_weight = self.conv.weight
+            conv_shape = conv_weight.shape
+            if conv_weight.is_cuda:
+                conv_weight_rd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 5 * 5).fill_(0)
+            else:
+                conv_weight_rd = torch.zeros(conv_shape[0], conv_shape[1], 5 * 5)
+            conv_weight = Rearrange('c_in c_out k1 k2 -> c_in c_out (k1 k2)')(conv_weight)
+            conv_weight_rd[:, :, [0, 2, 4, 10, 14, 20, 22, 24]] = conv_weight[:, :, 1:]
+            conv_weight_rd[:, :, [6, 7, 8, 11, 13, 16, 17, 18]] = -conv_weight[:, :, 1:] * self.theta
+            conv_weight_rd[:, :, 12] = conv_weight[:, :, 0] * (1 - self.theta)
+            conv_weight_rd = conv_weight_rd.view(conv_shape[0], conv_shape[1], 5, 5)
+            out_diff = nn.functional.conv2d(input=x, weight=conv_weight_rd, bias=self.conv.bias,
+                                            stride=self.conv.stride, padding=self.conv.padding, groups=self.conv.groups)
+
+            return out_diff
+
+
+class Conv2d_hd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False, theta=1.0):
+        super(Conv2d_hd, self).__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight_hd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 3 * 3).fill_(0)
+        conv_weight_hd[:, :, [0, 3, 6]] = conv_weight[:, :, :]
+        conv_weight_hd[:, :, [2, 5, 8]] = -conv_weight[:, :, :]
+        conv_weight_hd = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[2])(
+            conv_weight_hd)
+        return conv_weight_hd, self.conv.bias
+
+
+class Conv2d_vd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False):
+        super(Conv2d_vd, self).__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight_vd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 3 * 3).fill_(0)
+        conv_weight_vd[:, :, [0, 1, 2]] = conv_weight[:, :, :]
+        conv_weight_vd[:, :, [6, 7, 8]] = -conv_weight[:, :, :]
+        conv_weight_vd = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[2])(
+            conv_weight_vd)
+        return conv_weight_vd, self.conv.bias
+
+
+class DEConv(nn.Module):
+    def __init__(self, dim):
+        super(DEConv, self).__init__()
+        self.conv1_1 = Conv2d_cd(dim, dim, 3, bias=True)
+        self.conv1_2 = Conv2d_hd(dim, dim, 3, bias=True)
+        self.conv1_3 = Conv2d_vd(dim, dim, 3, bias=True)
+        self.conv1_4 = Conv2d_ad(dim, dim, 3, bias=True)
+        self.conv1_5 = nn.Conv2d(dim, dim, 3, padding=1, bias=True)
+
+    def forward(self, x):
+        w1, b1 = self.conv1_1.get_weight()
+        w2, b2 = self.conv1_2.get_weight()
+        w3, b3 = self.conv1_3.get_weight()
+        w4, b4 = self.conv1_4.get_weight()
+        w5, b5 = self.conv1_5.weight, self.conv1_5.bias
+
+        w = w1 + w2 + w3 + w4 + w5
+        b = b1 + b2 + b3 + b4 + b5
+        res = nn.functional.conv2d(input=x, weight=w, bias=b, stride=1, padding=1, groups=1)
+
+        return res
+
+
+class Detect_DEConvHead(nn.Module):
+    """YOLOv8 Detect head for detection models."""
+
+    dynamic = False  # force grid reconstruction
+    export = False  # export mode
+    end2end = False  # end2end
+    max_det = 300  # max_det
+    shape = None
+    anchors = torch.empty(0)  # init
+    strides = torch.empty(0)  # init
+
+    def __init__(self, nc=80, ch=()):
+        """Initializes the YOLOv8 detection layer with specified number of classes and channels."""
+        super().__init__()
+        self.nc = nc  # number of classes
+        self.nl = len(ch)  # number of detection layers
+        self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
+        self.no = nc + self.reg_max * 4  # number of outputs per anchor
+        self.stride = torch.zeros(self.nl)  # strides computed during build
+        c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], min(self.nc, 100))  # channels
+   # ------------------ 修改 cv2 模块 ------------------
+        self.cv2 = nn.ModuleList(
+            nn.Sequential(
+                DEConv(x),                    # 替换原始 Conv
+                Conv(x, c2, 1),               # 通道适配
+                Conv(c2, c2, 3),              # 保留原有结构
+                nn.Conv2d(c2, 4 * self.reg_max, 1)
+            ) for x in ch
+        )
+        # ------------------ 修改 cv3 模块 ------------------
+        self.cv3 = nn.ModuleList(
+            nn.Sequential(
+                nn.Sequential(
+                    DWConv(x, x, 3), 
+                    DEConv(x),                # 替换原始 Conv
+                    Conv(x, c3, 1)            # 通道适配
+                ),
+                nn.Sequential(
+                    DWConv(c3, c3, 3), 
+                    DEConv(c3),               # 替换原始 Conv
+                    Conv(c3, c3, 1)
+                ),
+                nn.Conv2d(c3, self.nc, 1),
+            ) for x in ch
+        )
+        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+
+        if self.end2end:
+            self.one2one_cv2 = copy.deepcopy(self.cv2)
+            self.one2one_cv3 = copy.deepcopy(self.cv3)
+
+    def forward(self, x):
+        """Concatenates and returns predicted bounding boxes and class probabilities."""
+        if self.end2end:
+            return self.forward_end2end(x)
+
+        for i in range(self.nl):
+            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+        if self.training:  # Training path
+            return x
+        y = self._inference(x)
+        return y if self.export else (y, x)
+
+    def forward_end2end(self, x):
+        """
+        Performs forward pass of the v10Detect module.
+
+        Args:
+            x (tensor): Input tensor.
+
+        Returns:
+            (dict, tensor): If not in training mode, returns a dictionary containing the outputs of both one2many and one2one detections.
+                           If in training mode, returns a dictionary containing the outputs of one2many and one2one detections separately.
+        """
+        # x_detach = [xi.detach() for xi in x]
+        one2one = [
+            torch.cat((self.one2one_cv2[i](x[i]), self.one2one_cv3[i](x[i])), 1) for i in range(self.nl)
+        ]
+        if hasattr(self, 'cv2') and hasattr(self, 'cv3'):
+            for i in range(self.nl):
+                x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+        if self.training:  # Training path
+            return {"one2many": x, "one2one": one2one}
+
+        y = self._inference(one2one)
+        y = self.postprocess(y.permute(0, 2, 1), self.max_det, self.nc)
+        return y if self.export else (y, {"one2many": x, "one2one": one2one})
+
+    def _inference(self, x):
+        """Decode predicted bounding boxes and class probabilities based on multiple-level feature maps."""
+        # Inference path
+        shape = x[0].shape  # BCHW
+        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
+        if self.dynamic or self.shape != shape:
+            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
+            self.shape = shape
+
+        if self.export and self.format in {"saved_model", "pb", "tflite", "edgetpu", "tfjs"}:  # avoid TF FlexSplitV ops
+            box = x_cat[:, : self.reg_max * 4]
+            cls = x_cat[:, self.reg_max * 4 :]
+        else:
+            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
+
+        if self.export and self.format in {"tflite", "edgetpu"}:
+            # Precompute normalization factor to increase numerical stability
+            # See https://github.com/ultralytics/ultralytics/issues/7371
+            grid_h = shape[2]
+            grid_w = shape[3]
+            grid_size = torch.tensor([grid_w, grid_h, grid_w, grid_h], device=box.device).reshape(1, 4, 1)
+            norm = self.strides / (self.stride[0] * grid_size)
+            dbox = self.decode_bboxes(self.dfl(box) * norm, self.anchors.unsqueeze(0) * norm[:, :2])
+        else:
+            dbox = self.decode_bboxes(self.dfl(box), self.anchors.unsqueeze(0)) * self.strides
+
+        return torch.cat((dbox, cls.sigmoid()), 1)
+
+    def bias_init(self):
+        """Initialize Detect() biases, WARNING: requires stride availability."""
+        m = self  # self.model[-1]  # Detect() module
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
+        # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
+        for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+            a[-1].bias.data[:] = 1.0  # box
+            b[-1].bias.data[: m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+        if self.end2end:
+            for a, b, s in zip(m.one2one_cv2, m.one2one_cv3, m.stride):  # from
+                a[-1].bias.data[:] = 1.0  # box
+                b[-1].bias.data[: m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+
+    def decode_bboxes(self, bboxes, anchors):
+        """Decode bounding boxes."""
+        return dist2bbox(bboxes, anchors, xywh=not self.end2end, dim=1)
+
+    @staticmethod
+    def postprocess(preds: torch.Tensor, max_det: int, nc: int = 80):
+        """
+        Post-processes YOLO model predictions.
+
+        Args:
+            preds (torch.Tensor): Raw predictions with shape (batch_size, num_anchors, 4 + nc) with last dimension
+                format [x, y, w, h, class_probs].
+            max_det (int): Maximum detections per image.
+            nc (int, optional): Number of classes. Default: 80.
+
+        Returns:
+            (torch.Tensor): Processed predictions with shape (batch_size, min(max_det, num_anchors), 6) and last
+                dimension format [x, y, w, h, max_class_prob, class_index].
+        """
+        batch_size, anchors, _ = preds.shape  # i.e. shape(16,8400,84)
+        boxes, scores = preds.split([4, nc], dim=-1)
+        index = scores.amax(dim=-1).topk(min(max_det, anchors))[1].unsqueeze(-1)
+        boxes = boxes.gather(dim=1, index=index.repeat(1, 1, 4))
+        scores = scores.gather(dim=1, index=index.repeat(1, 1, nc))
+        scores, index = scores.flatten(1).topk(min(max_det, anchors))
+        i = torch.arange(batch_size)[..., None]  # batch indices
+        return torch.cat([boxes[i, index // nc], scores[..., None], (index % nc)[..., None].float()], dim=-1)
+
+
