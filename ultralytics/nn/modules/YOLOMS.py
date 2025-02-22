f59e6a449612610cee9f37a46cfd4cb4c6b8ea01
scuzyq <142667711+scuzyq@users.noreply.github.com>
2025-02-22 13:15:54 +0800

    Create YOLOMS.py

---
+++ ultralytics/nn/modules/YOLOMS.py
@@ -0,0 +1,119 @@
+######################################## MS-Block start  by AI&CV ########################################
+ 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+ 
+ 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+ 
+ 
+class Bottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+ 
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+ 
+ 
+class C2f(nn.Module):
+    # CSP Bottleneck with 2 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+ 
+    def forward(self, x):
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+ 
+    def forward_split(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+ 
+ 
+class MSBlockLayer(nn.Module):
+    def __init__(self, inc, ouc, k) -> None:
+        super().__init__()
+ 
+        self.in_conv = Conv(inc, ouc, 1)
+        self.mid_conv = Conv(ouc, ouc, k, g=ouc)
+        self.out_conv = Conv(ouc, inc, 1)
+ 
+    def forward(self, x):
+        return self.out_conv(self.mid_conv(self.in_conv(x)))
+ 
+ 
+class MSBlock(nn.Module):
+    def __init__(self, inc, ouc, kernel_sizes, in_expand_ratio=3., mid_expand_ratio=2., layers_num=3,
+                 in_down_ratio=2.) -> None:
+        super().__init__()
+ 
+        in_channel = int(inc * in_expand_ratio // in_down_ratio)
+        self.mid_channel = in_channel // len(kernel_sizes)
+        groups = int(self.mid_channel * mid_expand_ratio)
+        self.in_conv = Conv(inc, in_channel)
+ 
+        self.mid_convs = []
+        for kernel_size in kernel_sizes:
+            if kernel_size == 1:
+                self.mid_convs.append(nn.Identity())
+                continue
+            mid_convs = [MSBlockLayer(self.mid_channel, groups, k=kernel_size) for _ in range(int(layers_num))]
+            self.mid_convs.append(nn.Sequential(*mid_convs))
+        self.mid_convs = nn.ModuleList(self.mid_convs)
+        self.out_conv = Conv(in_channel, ouc, 1)
+ 
+        self.attention = None
+ 
+    def forward(self, x):
+        out = self.in_conv(x)
+        channels = []
+        for i, mid_conv in enumerate(self.mid_convs):
+            channel = out[:, i * self.mid_channel:(i + 1) * self.mid_channel, ...]
+            if i >= 1:
+                channel = channel + channels[i - 1]
+            channel = mid_conv(channel)
+            channels.append(channel)
+        out = torch.cat(channels, dim=1)
+        out = self.out_conv(out)
+        if self.attention is not None:
+            out = self.attention(out)
+        return out
+ 
+ 
+ 
+ 
+######################################## MS-Block end   by AI&CV########################################
