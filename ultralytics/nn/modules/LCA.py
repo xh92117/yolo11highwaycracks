69d0009c7b9c5309417a1dae6880c567ede20877
scuzyq <142667711+scuzyq@users.noreply.github.com>
2025-02-26 15:43:24 +0800

    Create LCA.py

---
+++ ultralytics/nn/modules/LCA.py
@@ -0,0 +1,153 @@
+import torch.nn as nn
+import torch
+ 
+ 
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+ 
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+ 
+ 
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+ 
+    def forward(self, x):
+        return x * self.sigmoid(x)
+ 
+ 
+class LCA(nn.Module):
+    def __init__(self, input_channel, reduction=32):
+        super(LCA, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+ 
+        self.conv1 = nn.Conv2d(input_channel, input_channel, kernel_size=1,
+                               stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(input_channel)
+        self.act = h_swish()
+ 
+        self.conv_h = nn.Conv2d(input_channel, input_channel, kernel_size=1,
+                                stride=1, padding=0, groups=input_channel)
+        self.conv_w = nn.Conv2d(input_channel, input_channel, kernel_size=1,
+                                stride=1, padding=0, groups=input_channel)
+ 
+    def forward(self, x):  # torch.Size([2, 32, 64, 64])
+        identity = x
+ 
+        b, c, h, w = x.size()
+        x_h = self.pool_h(x)  # torch.Size([2, 32, 64, 1])
+        x_w = self.pool_w(x)  # torch.Size([2, 32, 1, 64])
+ 
+        a_h = self.conv_h(x_h).sigmoid()  # torch.Size([2, 32, 64, 1])
+        a_w = self.conv_w(x_w).sigmoid()  # torch.Size([2, 32, 1, 64])
+ 
+        out = identity * a_w * a_h  # torch.Size([2, 32, 64, 64])
+ 
+        return out
+ 
+ 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+ 
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+ 
+ 
+ 
+class PSABlock(nn.Module):
+    """
+    PSABlock class implementing a Position-Sensitive Attention block for neural networks.
+    This class encapsulates the functionality for applying multi-head attention and feed-forward neural network layers
+    with optional shortcut connections.
+    Attributes:
+        attn (Attention): Multi-head attention module.
+        ffn (nn.Sequential): Feed-forward neural network module.
+        add (bool): Flag indicating whether to add shortcut connections.
+    Methods:
+        forward: Performs a forward pass through the PSABlock, applying attention and feed-forward layers.
+    Examples:
+        Create a PSABlock and perform a forward pass
+        >>> psablock = PSABlock(c=128, attn_ratio=0.5, num_heads=4, shortcut=True)
+        >>> input_tensor = torch.randn(1, 128, 32, 32)
+        >>> output_tensor = psablock(input_tensor)
+    """
+ 
+    def __init__(self, c, attn_ratio=0.5, num_heads=4, shortcut=True) -> None:
+        """Initializes the PSABlock with attention and feed-forward layers for enhanced feature extraction."""
+        super().__init__()
+ 
+        self.attn = LCA(c)
+        self.ffn = nn.Sequential(Conv(c, c * 2, 1), Conv(c * 2, c, 1, act=False))
+        self.add = shortcut
+ 
+    def forward(self, x):
+        """Executes a forward pass through PSABlock, applying attention and feed-forward layers to the input tensor."""
+        x = x + self.attn(x) if self.add else self.attn(x)
+        x = x + self.ffn(x) if self.add else self.ffn(x)
+        return x
+ 
+ 
+class C2PSA_LCA(nn.Module):
+    """
+    C2PSA module with attention mechanism for enhanced feature extraction and processing.
+    This module implements a convolutional block with attention mechanisms to enhance feature extraction and processing
+    capabilities. It includes a series of PSABlock modules for self-attention and feed-forward operations.
+    Attributes:
+        c (int): Number of hidden channels.
+        cv1 (Conv): 1x1 convolution layer to reduce the number of input channels to 2*c.
+        cv2 (Conv): 1x1 convolution layer to reduce the number of output channels to c.
+        m (nn.Sequential): Sequential container of PSABlock modules for attention and feed-forward operations.
+    Methods:
+        forward: Performs a forward pass through the C2PSA module, applying attention and feed-forward operations.
+    Notes:
+        This module essentially is the same as PSA module, but refactored to allow stacking more PSABlock modules.
+    Examples:
+        >>> c2psa = C2PSA(c1=256, c2=256, n=3, e=0.5)
+        >>> input_tensor = torch.randn(1, 256, 64, 64)
+        >>> output_tensor = c2psa(input_tensor)
+    """
+ 
+    def __init__(self, c1, c2, n=1, e=0.5):
+        """Initializes the C2PSA module with specified input/output channels, number of layers, and expansion ratio."""
+        super().__init__()
+        assert c1 == c2
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+ 
+        self.m = nn.Sequential(*(PSABlock(self.c, attn_ratio=0.5, num_heads=self.c // 64) for _ in range(n)))
+ 
+    def forward(self, x):
+        """Processes the input tensor 'x' through a series of PSA blocks and returns the transformed tensor."""
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = self.m(b)
+        return self.cv2(torch.cat((a, b), 1))
+ 
